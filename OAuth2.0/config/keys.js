a9df570e943e0f2d336bfb38ecbb6804236a688b
ashutosh555 <ashutosh.nayak@suiit.ac.in>
2020-09-09 16:07:17 +0530

    Updated upto 09.09.2020

---
+++ OAuth2.0/config/keys.js
@@ -0,0 +1,14 @@
+//add this file to .gitignore
+
+module.exports = {
+  google: {
+    clientID: '993815087372-71gvgupq1k0fghcg1qvkqk8feqv9o024.apps.googleusercontent.com',
+    clientSecret: 'WZKLSp_j3f6JWk1MR_YmjRtd'
+  },
+  mongodb: {
+    dbURI: 'mongodb+srv://ashutosh:13795@oauth-test.wrgyi.mongodb.net/ashutosh?retryWrites=true&w=majority'
+  },
+  session: {
+    cookieKey: 'iamawesomyouknow'
+  }
+};
